d82f4eff502fedadb95bbda27083dd58fc33e620
cyx977 <cyx977@outlook.com>
2021-10-24 23:44:26 +0545

    decryption

--- src/caesar_cipher.py
+++ src/caesar_cipher.py
@@ -1,14 +1,18 @@
 #BRX DUH DZHVRPH 
 #key 3
 #YOU ARE AWESOME
+letters = "abcdefghijklmnopqrstuvwxyz";
 
-def genCaesar(n, string):
-    letters = "abcdefghijklmnopqrstuvwxyz";
+def genKey(n):
     key = {};
     count = 0;
     for c in letters:
         key[c] = letters[(count + n) % len(letters)]
         count += 1;
+    return key;
+
+def genCaesar(n, string):
+    key = genKey(n);
     caesar = "";
     for s in string:
         if(s not in key):
@@ -17,6 +21,15 @@ def genCaesar(n, string):
         caesar += key[s];
     return caesar;
 
-
+def decryptCaesar(n, string):
+    key = genKey(-n);
+    caesar = ""
+    for s in string:
+        if(s not in key):
+            caesar += s;
+            continue;
+        caesar += key[s];
+    return caesar;
     
 print(genCaesar(3,  "you are awesome"));
+print(decryptCaesar(3, genCaesar(3,  "you are awesome")));
