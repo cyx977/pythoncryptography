4466663134d306c1aa378d67978dc34367c9a384
cyx977 <cyx977@outlook.com>
2021-11-09 00:43:34 +0545

    hmac implementation

---
+++ src/hmac.py
@@ -0,0 +1,33 @@
+import hashlib
+import base64
+
+
+# Alice and Bob share a secret key
+secret_key = "secret key".encode()
+
+def modify(m):
+    l = list(m)
+    l[0] = l[0] ^ 1
+    return bytes(l)
+
+# Alice wants to compute a MAC
+m = "Hey Bob. You are still awesome.".encode()
+sha256 = hashlib.sha256()
+sha256.update(secret_key)
+sha256.update(m)
+hmac = sha256.digest()
+print(m, base64.b64encode(hmac))
+
+# eve comes along and modified the message
+m = modify(m)
+print(m)
+
+
+# Bob receives and validates the HMAC
+sha256 = hashlib.sha256()
+sha256.update(secret_key)
+sha256.update(m)
+hmac = sha256.digest()
+print(m, base64.b64encode(hmac))
+
+# bob finds a different hmac and finds out the message was tampered
